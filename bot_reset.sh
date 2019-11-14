96a9c5b36edb3ef951d76f9c07b1e4fdc583df10
Muhammad Azhdari <mohammad.azhdari.22@gmail.com>
2019-11-14 15:50:19 +0330

    improve informations

--- bot_reset.sh
+++ bot_reset.sh
@@ -1,9 +1,13 @@
 
-OUTPUT="$(sudo docker logs poll_bank 2>&1 | grep bot)"
+OUTPUT="$(sudo docker logs DOCKER CONTAINER NAME 2>&1 | grep LOG MESSAGE)"
 if [ -z "$OUTPUT" ]
 then
       echo "\$Error log wasn't found!"
+      date -u
+      echo $OUTPUT
 else
       echo "\$Error log was found!"
-      sudo docker-compose -f ~/PycharmProjects/polling_bot/docker-compose.yml restart
+    #   sudo docker-compose -f docker-compose.yml restart
+    date -u
+    echo $OUTPUT
 fi
